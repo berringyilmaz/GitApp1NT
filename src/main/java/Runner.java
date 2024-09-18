bb12d8dacb56592b60f3c6f6ba123c1c601cc559
Berrin Yilmaz <berringyilmaz@gmail.com>
2024-09-18 23:14:42 +0300

    yeniden ce

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -4,7 +4,6 @@ public class Runner {
         System.out.println("version 2");
         System.out.println("version 2 nin devami");
         System.out.println("version 3");
-        System.out.println("version 4");
 
 
 
